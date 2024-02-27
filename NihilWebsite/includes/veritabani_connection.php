d00d178d1a98fc4a9ad5f3f3c0e209320fa2a8c3
ziftgny <114746717+ziftgny@users.noreply.github.com>
2024-02-27 15:46:17 +0300

    deleted

--- NihilWebsite/includes/veritabani_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$database = mysqli_connect("localhost:3307","root","12345","proje");
-if(!$database){
-    echo "hata : ".mysqli_connect_error();
-}
-
-?>
--- includes/veritabani_connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$database = mysqli_connect("localhost","south","123456","proje");
-if(!$database){
-    echo "hata : ".mysqli_connect_error();
-}
-
-?>
